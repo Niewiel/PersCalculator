4d3258de18154888c00160c657e47a62b9948a0c
Tomasz Niewielski <tniewiel@gmail.com>
2018-08-01 21:57:27 +0200

    initial

---
+++ app/src/main/java/pl/com/pers/perscalculator/MainActivity.java
@@ -0,0 +1,85 @@
+package pl.com.pers.perscalculator;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ERROR = "pole wymagane";
+
+    private TextInputEditText pojemnosc;
+    private TextInputEditText cena;
+    private TextInputEditText stezenie;
+    private EditText ilocsLitrow;
+    private EditText cenaZaLitr;
+    private Button oblicz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindLayout();
+        oblicz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    policz(Double.parseDouble(pojemnosc.getText().toString()), Double.parseDouble(cena.getText().toString()), Double.parseDouble(stezenie.getText().toString()));
+                    hideKeyboard(getApplicationContext(),v);
+                }
+            }
+        });
+    }
+
+    private void bindLayout() {
+        pojemnosc = findViewById(R.id.pojemnosc);
+        cena = findViewById(R.id.cena);
+        stezenie = findViewById(R.id.stezenie_robocze);
+        ilocsLitrow = findViewById(R.id.gotowy_produkt);
+        cenaZaLitr = findViewById(R.id.cena_1l);
+        oblicz = findViewById(R.id.oblicz);
+
+    }
+
+    private boolean validate() {
+        int flag = -1;
+        System.err.println(flag);
+        if (pojemnosc.getText().toString().isEmpty()) {
+            pojemnosc.setError(ERROR);
+            flag++;
+            System.err.println(flag);
+        }
+        if (cena.getText().toString().isEmpty()) {
+            cena.setError(ERROR);
+            flag++;
+            System.err.println(flag);
+        }
+        if (stezenie.getText().toString().isEmpty()) {
+            stezenie.setError(ERROR);
+            flag++;
+            System.err.println(flag);
+        }
+
+        System.err.println(flag);
+        System.err.println(String.valueOf(flag < 0));
+        return (flag < 0);
+    }
+
+    private void policz(double pojemnosc, double cena, double stezenie) {
+        double ilosc = pojemnosc / (stezenie * 0.01);
+        ilocsLitrow.setText(String.valueOf(ilosc));
+        cenaZaLitr.setText(String.valueOf(cena / ilosc));
+        System.err.println(cenaZaLitr.getText().toString());
+    }
+
+    public static void hideKeyboard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+}
